0a330506d79a4b113ceca4b0e2e7e20aad2ee94a
sasangachathumal <chathumal.sasanga02@gmail.com>
2018-03-28 15:29:24 +0530

    add method to click input.

--- src/profile/profile.controller.js
+++ src/profile/profile.controller.js
@@ -6,17 +6,43 @@
     .module('practeraChat.profile')
     .controller('profileController', profileController);
 
-  profileController.$inject = ['authDataService', '$state', 'cookieManagerService', '$ionicHistory', 'profileService'];
+  profileController.$inject = ['authDataService', '$location', 'cookieManagerService', '$ionicHistory', 'profileService'];
 
-  function profileController(_authDataService, $state, _cookieManagerService, $ionicHistory, _profileService) {
+  function profileController(_authDataService, $location, _cookieManagerService, $ionicHistory, _profileService) {
     let vm = this;
 
     vm.user = _cookieManagerService.getUserCookie();
+    vm.userForEdit = {};
 
     vm.goToBackView = goToBackView;
+    vm.imageSelecter = imageSelecter;
+
+    if ($location.search()['type']) {
+      vm.userForEdit = vm.user;
+    } else {
+      activate();
+    }
+
+    function getProfileOnSuccessCallback(response) {
+      vm.user = response.data.data['User'];
+      _profileService.setUserProfile(vm.user);
+    }
+
+    function getProfileOnErrorCallback(error) {
+      console.error(error);
+    }
+
+    function activate() {
+      _profileService.getMyProfile(getProfileOnSuccessCallback, getProfileOnErrorCallback);
+    }
 
     function goToBackView() {
       $ionicHistory.goBack();
+    }
+
+    function imageSelecter(id) {
+      alert("iydgsfh");
+      document.getElementById(id).click();
     }
 
   }
--- src/profile/profile.route.js
+++ src/profile/profile.route.js
@@ -9,12 +9,14 @@
   function config($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('profile', {
+        cache: false,
         url: '/profile',
         templateUrl: 'templates/profile/profile-view.html',
         controller: 'profileController',
         controllerAs: 'profileCtrl'
       })
       .state('profileEdit', {
+        cache: false,
         url: '/profileEdit',
         templateUrl: 'templates/profile/profile-edit.html',
         controller: 'profileController',
