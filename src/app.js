15ce389fb8b2fec7d519a5204794207bbe9c7725
sasangachathumal <chathumal.sasanga02@gmail.com>
2018-03-29 09:00:08 +0530

    added fcm plugin and cordova config.

--- src/app.js
+++ src/app.js
@@ -9,21 +9,27 @@
 
   angular.module('practeraChat', ['ionic', 'ionic.ion.autoListDivider', 'ngCookies', 'practeraChat.chat', 'practeraChat.group', 'practeraChat.message', 'practeraChat.auth', 'practeraChat.config', 'practeraChat.createChat'])
 
-    .run(function ($ionicPlatform) {
+    .run(function ($ionicPlatform, cordovaService) {
       $ionicPlatform.ready(function () {
         if (window.cordova && window.cordova.plugins.Keyboard) {
           // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
           // for form inputs)
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          // cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
           // Don't remove this line unless you know what you are doing. It stops the viewport
           // from snapping when text inputs are focused. Ionic handles this internally for
           // a much nicer keyboard experience.
-          cordova.plugins.Keyboard.disableScroll(true);
+          // cordova.plugins.Keyboard.disableScroll(true);
+
+          alert("hdjheuhe");
+          console.log("hdjheuhe");
+          cordovaService.initialize();
         }
         if (window.StatusBar) {
           StatusBar.styleDefault();
         }
+
+
       });
     })
     .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
---
+++ src/services/fcm.service.js
@@ -0,0 +1,42 @@
+/**
+ * Created by sasangachathumal on 3/23/18.
+ */
+(function () {
+  angular
+    .module('practeraChat')
+    .factory('fcmFactory', fcmFactory);
+
+  fcmFactory.$inject = [];
+
+  function fcmFactory() {
+    return {
+      getToken: getToken,
+      refreshToken: refreshToken,
+      onNotification: onNotification
+    };
+
+    function getToken(callback) {
+      FCMPlugin.getToken(callback);
+    }
+
+    function refreshToken() {
+      FCMPlugin.onTokenRefresh(function (token) {
+        return token;
+      });
+    }
+
+    function onNotification() {
+      FCMPlugin.onNotification(function (data) {
+        if (data.wasTapped) {
+          //Notification was received on device tray and tapped by the user.
+          // alert( JSON.stringify(data) );
+          Notification.primary('Primary notification');
+
+        } else {
+          //Notification was received in foreground. Maybe the user needs to be notified.
+          Notification.primary('Primary notification');
+        }
+      });
+    }
+  }
+})();
