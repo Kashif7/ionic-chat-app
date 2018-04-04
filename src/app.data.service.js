14e4a7e0a108321fc8d68aa6210c7ff2bc6aead7
sasangachathumal <chathumal.sasanga02@gmail.com>
2018-04-04 14:49:13 +0530

    update push notification logic.

--- src/app.data.service.js
+++ src/app.data.service.js
@@ -22,33 +22,39 @@
 
     function showNotification(data) {
       console.log("showNotification", data);
-      var notification = {
-        template: '<p ng-click="goToChat()">New message<br> from <small>{{title}}</small></p>',
-        position: 'top right',
-        scope: {
-          title: data.notification.title,
-          goToChat: function () {
-            let user = JSON.parse(localStorage.getItem('user'));
-            let threadId = data.data.tag;
-            let thread;
 
-            let refString = `/threads/${user.id}/${threadId}`;
-            let ref = firebase.database()
-              .ref(refString);
+      let user = JSON.parse(localStorage.getItem('user'));
+      let threadId;
+      if (data.tag) {
+        threadId = data.tag;
+      } else {
+        threadId = data.data.tag;
+      }
+      let thread;
 
-            $firebaseObject(ref)
-              .$loaded()
-              .then((newThread) => {
-                thread = newThread;
+      let refString = `/threads/${user.id}/${threadId}`;
+      let ref = firebase.database()
+        .ref(refString);
+
+      $firebaseObject(ref)
+        .$loaded()
+        .then((newThread) => {
+          thread = newThread;
+          var notification = {
+            template: '<p ng-click="goToChat()">New message<br> from <small>{{title}}</small></p>',
+            position: 'top right',
+            scope: {
+              title: thread.displayName,
+              goToChat: function () {
                 _messageDataService.setThread(thread);
                 $window.location.href = (`#/chat-messages?type=${thread.type}`);
-              });
-          }
-        },
-        hasDelay: true,
-        delay: 3000,
-      };
-      notifier.notify(notification);
+              }
+            },
+            hasDelay: true,
+            delay: 3000,
+          };
+          notifier.notify(notification);
+        });
     }
 
   }
